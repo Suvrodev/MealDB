439bd2f55806b525378faaddc36fbfad461fc001
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-03-02 00:55:12 +0600

    Added Modal

--- JS/indexdev.js
+++ JS/indexdev.js
@@ -6,6 +6,26 @@ const LoadMeals=(url)=>{
     .then(Data=>DisplayMeals(Data.meals))
 }
 
+const LoadOneMeal=(url)=>{
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>OneMealDetail(data.meals[0]))
+}
+
+const OneMealDetail= Data=>{
+    console.log("ID: "+Data.idMeal)
+    console.log(Data)
+    document.getElementById('MealDetailsModalLabel').innerText=Data.strMeal
+    document.getElementById('mealpic').innerHTML=`
+     <img class="img-fluid" 150px src="${Data.strMealThumb}" >
+    `
+}
+
+const LoadSpecificmeal=(Data)=>{
+    console.log(Data)
+    LoadOneMeal(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${Data}`)
+}
+
 const DisplayMeals=(Data)=>{
     const MealsContainer=document.getElementById('meals_container');
     MealsContainer.innerHTML=''
@@ -21,6 +41,10 @@ const DisplayMeals=(Data)=>{
                 <div class="card-body">
                 <h5 class="card-title"> ${data.strMeal} </h5>
                 <p class="card-text"> ${data.strInstructions} </p>
+
+                <button type="button" onclick="LoadSpecificmeal('${data.idMeal}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#MealDetailsModal">
+                  Show Details
+                </button>
                 </div>
             </div>
         `
